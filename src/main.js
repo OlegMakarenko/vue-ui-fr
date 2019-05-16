79f8f9c4067d799150f6d174e43daf1497d4a8f6
NKolva <radik061@gmail.com>
2019-05-16 12:05:17 +0300

    for using element-ui index.css, u need install url-loader and add a few lines of code to webpack.config

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router' 
 //import VueTouch from 'vue-touch'
 import ElementUI from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css";
 
 
 import store from './store'
